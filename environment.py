a1bcdb7b1a917c2788082b4bf39eb96bfb37287d
Sagnik Anupam <sagnikanupam@gmail.com>
2023-04-26 20:37:09 -0400

    map bug fix

--- environment.py
+++ environment.py
@@ -413,7 +413,7 @@ def data_eval(environment, data_path, output_path, model_path):
                 for st in model.recover_solutions(history)[0]:
                     print(st.facts[-1])
                 #eq_output = '> '.join(map(lambda s: f'{s.facts[-1]} | {s.parent_action and s.parent_action.action}', model.recover_solutions(history)[0]))
-                eq_output = ' | '.join(map(lambda s: f'{s.facts[-1]}'), model.recover_solutions(history)[0])
+                eq_output = ' | '.join(map(lambda s: f'{s.facts[-1]}', model.recover_solutions(history)[0]))
                 print(eq_output)
                 output_df.loc[len(output_df.index)] = [str(i), eq_output]
     
